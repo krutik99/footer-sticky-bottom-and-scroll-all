ac8f6e2418c879a6577bc790d8c617c3467e7464
Krutik Goyani <103425190+krutik99@users.noreply.github.com>
2023-07-19 22:25:44 +0530

    Added Project

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <header style={{ backgroundColor: "pink" }}>
+          Your header content here
+        </header>
+        <main style={{ flexGrow: 1, overflowY: "auto" }}>
+          Your main content here ddd ddd dsd
+          {/* <p>ccssc</p>
+ <p>ccssc</p>
+ <p>ccssc</p>
+ <p>ccssc</p>
+ <p>ccssc</p>
+ 
+ <p>ccssc</p>
+ <p>ccssc</p>
+ <p>ccssc</p>
+ <p>ccssc</p>
+ <p>ccssc</p>
+ <p>ccssc</p>
+ <p>ccssc</p>
+ <p>ccssc</p>
+ <p>ccssc</p>
+ <p>ccssc</p> */}
+          <p>ccssc</p>
+          <p>ccssc</p>
+          <p>ccssc</p>
+          <p>ccssc</p>
+          <p>ccssc</p>
+        </main>
+        <footer
+          style={{ backgroundColor: "#f1f1f1", padding: "20px", bottom: 0 }}
+        >
+          Your footer content here
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
